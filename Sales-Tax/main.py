6426effbe5a7695e28ee97421f1a3585f15a9b9d
Jordyn Luna <jordynluna@westernwyoming.edu>
2024-09-04 13:11:37 -0600

    Add files via upload

--- Sales-Tax/main.py
+++ Sales-Tax/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Jordyn Luna
+# 09/04/2024
 # Sales Tax Programming Project
 # COSC 2409 DNT
 #
@@ -10,13 +10,14 @@
 # Constants for the state and county tax rates
 
 # Get the amount of the purchase.
-
+amount_of_purchase = float(input('Enter the amount of the purchase: '))
 # Calculate the state sales tax.
-
+state_sales_tax = amount_of_purchase * 0.05
 # Calculate the county sales tax.
-
+county_sales_tax = amount_of_purchase * 0.025
 # Calculate the total tax.
-
+total_tax = state_sales_tax + county_sales_tax
 # Calculate the total of the sale.
-
+TotalOfTheSale = amount_of_purchase + total_tax
 # Print information about the sale.
+print('The total is $', format(TotalOfTheSale, ',.2f'))
