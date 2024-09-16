1b05acc20cc3f8975303bfa6ce4bd592f703cb0a
Jordyn Luna <jordynluna@westernwyoming.edu>
2024-09-16 12:54:09 -0600

    Add files via upload

--- Bug-Collector/main.py
+++ Bug-Collector/main.py
@@ -1,11 +1,23 @@
 #
-# Name
-# Date
+# Jordyn Luna
+# 09/16/2024
 # Bug Collector Programming Project
 # COSC 2409 DNT
 #
 # Initialize variables for bugs and total number of bugs collected.
+bugs = 0
+total = 0
 
 # Get number of bugs collected each day using a for loop.
+for day in range(1, 6):
+    # Prompt the user.
+    print('Enter the bugs collected on day', day)
+
+    # Input the number of bugs.
+    bugs = int(input())
+
+    # Add bugs to total.
+    total += bugs
 
 # Display the total number of bugs collected.
+print('You collected a total of', total, 'bugs.')
