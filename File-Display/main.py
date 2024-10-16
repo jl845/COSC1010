da1621b8342dcef8b305e394683be1a13b2c4ee0
Jordyn Luna <jordynluna@westernwyoming.edu>
2024-10-16 15:01:17 -0600

    Add files via upload

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,26 @@
 #
-# Name
-# Date
+# Jordyn Luna
+# 10/16/2024
 # File Display Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+def main():
+    # Declare local variables
+    contents = ''
+
+    # Open numbers.txt file for reading
+    infile = open('numbers.txt', 'r')
+
+    # Read in data and store in content
+    contents = infile.read()
+
+    # Close file
+    infile.close()
+
+    # Print contents
+    print(contents)
+
+# Call the main function
+main()    
