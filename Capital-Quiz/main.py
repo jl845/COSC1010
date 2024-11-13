2c8d8cb75362d96018cf89162fdcfb49979e6d34
Jordyn Luna <jordynluna@westernwyoming.edu>
2024-11-13 10:16:22 -0700

    Add files via upload

--- Capital-Quiz/main.py
+++ Capital-Quiz/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Jordyn Luna
+# 11/12/2024
 # Capital Quiz Programming Project
 # COSC 2409 DNT
 #
@@ -37,8 +37,36 @@ def main():
                 'Wisconsin':'Madison', 'Wyoming':'Cheyenne'}
 
     # Local variables
+    correct = 0
+    incorrect = 0
 
     # Continue until user quits the game.
+    print('Welcome to the Capital Quiz! You will be tested on all 50 states and then given your score. If you want to quit before then, enter quit.')
+
+    for count in range(50):
+        # Get a random entry from the dictionary
+        state, capital = capitals.popitem()
+
+        # Quiz the user
+        print('What is the capital of', state, '?', end=' ')
+        response = input()
+
+        # Is the user correct?
+        if response.lower() == 'quit':
+            print('Good job!') 
+            break
+
+        if response.lower() == capital.lower():
+            correct += 1
+            print ('Correct!')
+        else:
+            incorrect += 1
+            print('Incorrect.')  
+
+    # Display the results
+    print(f'\nFinal score:')
+    print('Correct responses:', correct)
+    print('Incorrect responses, incorrect')             
 
 # Call the main function.
 main()
